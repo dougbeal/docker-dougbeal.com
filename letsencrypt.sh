af982f76677b61117c6379137e0178d163f450aa
Douglas Beal <public.git@dougbeal.com>
2018-06-25 13:08:16 -0700

    generate dhparam.pem if it doesn't exist

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 docker-compose -f docker-compose-letsencrypt.yml up
-    
+dhparam_file="/home/dockerrun/docker-dougbeal.com/volumes/letsencrypt/nginx-certs/dhparam.pem"
+if [ ! -f $dhparam_file ]; then
+    openssl dhparam -out $dhparam_file 4096
+fi
