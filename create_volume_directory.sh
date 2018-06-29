4a8cd4b859afbf987a8b12636b1660cc75804af2
Douglas Beal <public.git@dougbeal.com>
2018-06-29 12:22:34 -0700

    add together directory

--- create_volume_directory.sh
+++ create_volume_directory.sh
@@ -9,6 +9,7 @@ mkdir -p volumes/import-database/
 mkdir -p volumes/import-wordpress/
 mkdir -p volumes/letsencrypt/
 mkdir -p volumes/well-known/
-mkdir -p volumes/database
+mkdir -p volumes/database/
+mkdir -p volumes/together/
 
 chown -R docksquash:docksquash volumes/*
