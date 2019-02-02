2c9e266955daba0c0adc84dd6fb05a9b45d1dd83
Douglas Beal <public.git@dougbeal.com>
2019-02-02 10:23:40 -0800

    don't screw up database permissions

--- create_volume_directory.sh
+++ create_volume_directory.sh
@@ -16,5 +16,7 @@ mkdir -p volumes/openspace/
 
 chown -R docksquash:docksquash volumes/*
 chown -R dock-www-data:dock-www-data volumes/${SITE}/wp-content/
+chown -R 166535 -R volumes/database
+chown 166535 -R *
 find volumes/ -name .gitignore -exec chmod g+rw {} \;
 find volumes/ -type d -exec chmod g+rw {} \;
