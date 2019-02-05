bdff75f2609649eecccca15413af78ceee9a4d14
Douglas Beal <public.git@dougbeal.com>
2019-02-04 17:40:44 -0800

    tighten up directory owners

--- create_volume_directory.sh
+++ create_volume_directory.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -x
 export $(cat .env | xargs)
-
+# require variables to be set
+set -euo pipefail
 
 mkdir -p volumes/${SITE}
 mkdir -p volumes/${SITE}_d/themes
@@ -14,9 +15,14 @@ mkdir -p volumes/together/
 mkdir -p volumes/openspace/
 
 
-chown -R docksquash:docksquash volumes/*
-chown -R dock-www-data:dock-www-data volumes/${SITE}/wp-content/
-chown -R 166535 -R volumes/database
-chown 166535 -R *
+# general volumes owner is docksquash:docksquash
+find ./volumes -type d \( -path ${SITE} database \) -prune -o -print0 | xargs -0 chown docksquash:docksquash
+# wp-content is writable by being owned by dock-www-data:dock-www-data
+find ./volumes/${SITE}/wp-content/ -type d \( -path BANANA_FOR_SCALE \) -prune -o -print0 | xargs -0 chown dock-www-data:dock-www-data
+# database is writble by docker-root (166535)
+find ./volumes/dataase/ -type d \( -path BANANA_FOR_SCALE \) -prune -o -print0 chown 166535:166535 -R volumes/database
+find . -type d \( -path volumes \) -prune -o -print0 | xargs -0 chown dockerrun:dockerrun
+# make sure .gitgnore is writeble
 find volumes/ -name .gitignore -exec chmod g+rw {} \;
+# make sure directories are traversable
 find volumes/ -type d -exec chmod g+rw {} \;
