865edaad3392394e76fa4218b4d85f17291ae52d
Douglas Beal <public.git@dougbeal.com>
2019-02-04 22:36:47 -0800

    fixed permission

--- create_volume_directory.sh
+++ create_volume_directory.sh
@@ -15,17 +15,29 @@ mkdir -p volumes/together/
 mkdir -p volumes/openspace/
 
 
+
 # general volumes owner is docksquash:docksquash
-find ./volumes -type d \( -path ${SITE} database \) -prune -o -print0 | xargs -0 chown docksquash:docksquash
+find ./volumes -maxdepth 1 -type d \( -path ./volumes/${SITE} -o -path ./volumes/database \) -prune -o -print0 | \
+    xargs -0 chown -R docksquash:docksquash
+
+
+# non wp-content
+find ./volumes/${SITE} -maxdepth 1 -type d \( -path ./volumes/${SITE}/wp-content \) -prune -o -print0 | \
+    xargs -0 chown -R docksquash:docksquash
 
 # wp-content is writable by being owned by dock-www-data:dock-www-data
-find ./volumes/${SITE}/wp-content/ -type d \( -path BANANA_FOR_SCALE \) -prune -o -print0 | xargs -0 chown dock-www-data:dock-www-data
+find ./volumes/${SITE}/wp-content/ -maxdepth 1 -type d \( -path BANANA_FOR_SCALE \) -prune -o -print0 | \
+    xargs -0 chown -R dock-www-data:dock-www-data
 
-# database is writable by docker-root (mapped to uid 166535 on host)
-find ./volumes/database/ -type d \( -path BANANA_FOR_SCALE \) -prune -o -print0 | xargs -0 chown 166535:166535
+# database is writable by container mysql (uid 999) (mapped to uid 999+docksquash = 166535 on host)
+find ./volumes/database/ -maxdepth 1 -type d \( -path BANANA_FOR_SCALE \) -prune -o -print0 | \
+    xargs -0 chown -R $(( $(id -u docksquash ) + 999 )):$(( $(id -g docksquash ) + 999 ))
+
 
 # everything excluding volumes is owned by dockerrun and stored in git
-find . -type d \( -path volumes \) -prune -o -print0 | xargs -0 chown dockerrun:dockerrun
+find .  -maxdepth 1 -type d \( -path volumes \) -prune -o -print0 | \
+    xargs -0 -R chown dockerrun:dockerrun
+
 # make sure .gitgnore is writeble
 find volumes/ -name .gitignore -exec chmod g+rw {} \;
 # make sure directories are traversable
