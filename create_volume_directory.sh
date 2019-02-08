62d4044966cafb96abbde684f3d9b100f6a21c7c
Douglas Beal <public.git@dougbeal.com>
2019-02-07 20:21:00 -0800

    exclude root directory when searching with -min-depth=1, don't use find when there are no directories being exclude

--- create_volume_directory.sh
+++ create_volume_directory.sh
@@ -17,30 +17,27 @@ mkdir -p volumes/openspace/
 
 
 # general volumes owner is docksquash:docksquash
-find ./volumes -maxdepth 1 -type d \( -path ./volumes/${SITE} -o -path ./volumes/database \) -prune -o -print0 | \
+find ./volumes -mindepth 1 -maxdepth 1 -type d \( -path ./volumes/${SITE} -o -path ./volumes/database \) -prune -o -print0 | \
     xargs -0 chown -R docksquash:docksquash
 
 
 # non wp-content
-find ./volumes/${SITE} -maxdepth 1 -type d \( -path ./volumes/${SITE}/wp-content \) -prune -o -print0 | \
+find ./volumes/${SITE}  -mindepth 1 -maxdepth 1 -type d \( -path ./volumes/${SITE}/wp-content \) -prune -o -print0 | \
     xargs -0 chown -R docksquash:docksquash
 
 # wp-content is writable by being owned by dock-www-data:dock-www-data
-find ./volumes/${SITE}/wp-content/ -maxdepth 1 -type d \( -path BANANA_FOR_SCALE \) -prune -o -print0 | \
-    xargs -0 chown -R dock-www-data:dock-www-data
+chown -R dock-www-data:dock-www-data ./volumes/${SITE}/wp-content/
 
-find ./volumes/openspace/public/ -maxdepth 1 -type d \( -path BANANA_FOR_SCALE \) -prune -o -print0 | \
-    xargs -0 chown -R dock-www-data:dock-www-data
+chown -R dock-www-data:dock-www-data ./volumes/openspace/public/
 
 
 
 # database is writable by container mysql (uid 999) (mapped to uid 999+docksquash = 166535 on host)
-find ./volumes/database/ -maxdepth 1 -type d \( -path BANANA_FOR_SCALE \) -prune -o -print0 | \
-    xargs -0 chown -R $(( $(id -u docksquash ) + 999 )):$(( $(id -g docksquash ) + 999 ))
+chown -R $(( $(id -u docksquash ) + 999 )):$(( $(id -g docksquash ) + 999 )) ./volumes/database/
 
 
 # everything excluding volumes is owned by dockerrun and stored in git
-find .  -maxdepth 1 -type d \( -path volumes \) -prune -o -print0 | \
+find .  -mindepth 1 -maxdepth 1 -type d \( -path volumes \) -prune -o -print0 | \
     xargs -0 chown dockerrun:dockerrun -R 
 
 # make sure .gitgnore is writeble
