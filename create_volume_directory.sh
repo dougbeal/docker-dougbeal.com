a828e86f0ed9f2b425cde83f4ef7029d0cc37180
Douglas Beal <public.git@dougbeal.com>
2019-03-16 07:01:31 -0700

    .git owned by dockerrun

--- create_volume_directory.sh
+++ create_volume_directory.sh
@@ -56,6 +56,8 @@ chown dock-www-data:dock-www-data ./volumes/org.foolscap.podcast/media/
 
 chown -R docksquash:docksquash ./volumes/org.foolscap.podcast/themes/*/assets/
 
+# git directeories are managed by dockerrun:dockerrun
+find /home/dockerrun/docker-dougbeal.com/volumes/ -name .git -type d -print -execdir chown dockerrun:dockerrun -R .git \;
 
 # database is writable by container mysql (uid 999) (mapped to uid 999+docksquash = 166535 on host)
 chown -R $(( $(id -u docksquash ) + 999 )):$(( $(id -g docksquash ) + 999 )) ./volumes/database/
